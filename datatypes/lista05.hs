2ac376f8a92a0c7917e1b6177e9682021ad30a3e
Thaynnara007 <thaynnara.goncalves@ccc.ufcg.edu.br>
2019-04-08 19:45:40 -0300

    add lista05

---
+++ datatypes/lista05.hs
@@ -0,0 +1,80 @@
+--Escreva a declaracao para o tipo Triple, contendo tres elementos, todos de tipos diferentes.
+--Escreva funcoes tripleFst, tripleSnd, tripleThr para extrair respectivamente o primeiro, segundo e terceiro
+-- elementos de uma triple.
+data Triple a b c = Nada deriving (Eq,Show)
+
+tripleFst = undefined
+tripleSnd = undefined
+tripleThr = undefined
+
+--Escreva um tipo Quadruple que contem 4 elementos: dois de um mesmo tipo e outros dois de outro tipo
+--Escreva as funcoes frstTwo e secondTwo que retornam os dois primeiros e os dois ultimos, respectivamente
+data Quadruple a b = Vazio
+
+firstTwo = undefined
+secondTwo = undefined
+
+--Escreva um tipo de dados que pode conter um, dois, tres ou quatro elementos, dependendo do construtor
+--Implemente funções tuple1 até tuple4 que que retornam Just <valor> ou Nothing se o valor nao existe
+data Tuple a b c d = NVazio
+
+tuple1 = undefined 
+tuple2 = undefined 
+tuple3 = undefined 
+tuple4 = undefined 
+
+data List a = Nil | Cons a (List a) deriving (Eq,Show)
+
+listLength Nil = 0
+listLength (Cons x xs) = 1 + listLength xs
+
+listHead Nil = error "Empty list"
+listHead (Cons x xs) = x
+
+listTail Nil = error "Empty list"
+listTail (Cons x xs) = xs
+
+listFoldr f v Nil = v
+listFoldr f v (Cons x xs) = f x (listFoldr f v xs)
+
+
+listFoldl f v Nil = v
+listFoldl f v (Cons x xs) = listFoldl f (f v x) xs 
+
+--Escreva as funcoes sobre a estrutura de dados binary tree
+data BinaryTree a = NIL | Node a (BinaryTree a) (BinaryTree a)
+ deriving (Eq,Show)
+
+sizeBST NIL = 0
+sizeBST (Node a left right) = 1 + sizeBST left + sizeBST right
+
+--verifica se uma BT é uma BST
+isBST = undefined
+
+--insere uma nova chave na BST retornando a BST modificada
+insert = undefined
+
+--retorna o Node da BST contendo o dado procurado ou entao NIL
+search = undefined
+
+--retorna o elmento maximo da BST
+maximum = undefined
+
+--retorna o elemento minimo da BST
+minimum = undefined
+
+--retorna o predecessor de um elemento da BST, caso o elemento esteja na BST
+predecessor = undefined
+
+--retorna o sucessor de um elemento da BST, caso o elemento esteja na BST
+successor = undefined
+
+--remove ume lemento da BST
+remove = undefined
+
+--retorna uma lista com os dados da BST nos diversos tipos de caminhamento
+preOrder = undefined
+order = undefined
+postOrder = undefined
+
+tipos-de-dados.hs
