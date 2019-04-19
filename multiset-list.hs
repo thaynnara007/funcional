d83b958d75e36131d023e6a30abcfe5fb3c6aeab
Thaynnara007 <thaynnara.goncalves@ccc.ufcg.edu.br>
2019-04-19 19:36:09 -0300

    add intersection_bags

--- multiset-list.hs
+++ multiset-list.hs
@@ -91,9 +91,18 @@ union_bags (Bag list1_b1 list2_b1) (Bag list1_b2 list2_b2) = Bag new_list1 new_l
 {-
  - Faz a intersecao deste Bag com otherBag. A intersecao consiste em ter os elementos que estao em ambos os bags com suas 
  - menores quantidades. Por exemplo, Seja A = {(a,3),(b,1)} e B = {(a,1)}. Assim, A.intersection(B) deixa A = {(a,1)}
- - Caso senhum elemento de A esteja contido em B ent�o a intersecao deixa A vazio.
+ - Caso nenhum elemento de A esteja contido em B ent�o a intersecao deixa A vazio.
 -}
-intersection bag1 bag2 = undefined
+intersection_amount [] (Bag list1_b1 list2_b1) (Bag list1_b2 list2_b2) = []
+intersection_amount (x:xs) (Bag list1_b1 list2_b1) (Bag list1_b2 list2_b2) = (min amount_b1 amount_b2):(intersection_amount xs (Bag list1_b1 list2_b1) (Bag list1_b2 list2_b2))
+  where
+    amount_b1 = search x (Bag list1_b1 list2_b1)
+    amount_b2 = search x (Bag list1_b2 list2_b2)
+
+intersection_bags (Bag list1_b1 list2_b1) (Bag list1_b2 list2_b2) = Bag new_list1 new_list2
+    where
+      new_list1 = intersect list1_b1 list1_b2
+      new_list2 = intersection_amount new_list1 (Bag list1_b1 list2_b1) (Bag list1_b2 list2_b2)
 
 {-
  - Faz a diferenca deste Bag com otherBag. A diferenca A \ B entre bags eh definida como segue:
