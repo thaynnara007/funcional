072fcf4c3d9ad745074e1f24cbba8ba14a130bb9
Thaynnara007 <thaynnara.goncalves@ccc.ufcg.edu.br>
2019-04-20 12:55:20 -0300

    adding some functions

--- multiset-map.hs
+++ multiset-map.hs
@@ -11,6 +11,7 @@ module MultisetMap ()
  - Eh recomendavel que voce consulte a documentacao de Data.Map
  -}
 import Data.Map as Map
+import Data.Char
 
 from_just (Just a) = a
 
@@ -44,14 +45,15 @@ search_bag elem bag = Map.lookup elem bag
  - Faz a uniao deste Bag com otherBag. A uniao consiste em ter os elementos dos dois Bags com suas maiores quantidades.
  - Por exemplo, A = {(a,1),(c,3)}, B = {(b,2),(c,1)}. A.union(B) deixa A = {(a,1),(c,3),(b,2)}
 -}
---union bag1 bag2 = undefined
+
+union_bag bag1 bag2 = Map.unionWith (max) bag1 bag2
 
 {-
  - Faz a intersecao deste Bag com otherBag. A intersecao consiste em ter os elementos que estao em ambos os bags com suas 
  - menores quantidades. Por exemplo, Seja A = {(a,3),(b,1)} e B = {(a,1)}. Assim, A.intersection(B) deixa A = {(a,1)}
  - Caso senhum elemento de A esteja contido em B ent�o a intersecao deixa A vazio.
 -}
---intersection bag1 bag2 = undefined
+intersection_bag bag1 bag2 = Map.intersectionWith (min) bag1 bag2
 
 {-
  - Faz a diferenca deste Bag com otherBag. A diferenca A \ B entre bags eh definida como segue:
@@ -60,7 +62,9 @@ search_bag elem bag = Map.lookup elem bag
      Caso essa quantidade seja negativa o elemento deve serremovido do Bag. 
      Por exemplo, seja A = {(a,3),(b,1)} e B = {(b,2),(a,1)}. Assim, A.minus(B) deixa A = {(a,2)}.
 -}
---minus bag1 bag2 = undefined
+
+f al ar = if (al-ar) > 0 then Just (al-ar) else Nothing      
+minus bag1 bag2 = Map.differenceWith f bag1 bag2
 
 {-
  - Testa se este Bag esta incluso em otherBag. Para todo elemento deste bag, sua quantidade
@@ -71,9 +75,9 @@ search_bag elem bag = Map.lookup elem bag
 {-
  - Realiza a soma deste Bag com otherBag. A soma de dois bags contem os elementos dos dois bags com suas quantidades somadas. 
 -}
---sum bag1 bag2 = undefined
+sum_bag bag1 bag2 = Map.unionWith (+) bag1 bag2
 
 {-
  - Retorna a quantidade total de elementos no Bag
 -}
---size bag = undefined
+size_bag bag = Map.size bag
