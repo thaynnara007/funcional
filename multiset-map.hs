0034738f27a7636d2e79d6137b3726926dfa0afe
Thaynnara007 <thaynnara.goncalves@ccc.ufcg.edu.br>
2019-04-20 11:49:31 -0300

    add insert_bag

--- multiset-map.hs
+++ multiset-map.hs
@@ -10,36 +10,42 @@ module MultisetMap ()
  - cada elemento da lista consiste do dado em si mapeado para sua quantidade. 
  - Eh recomendavel que voce consulte a documentacao de Data.Map
  -}
- import qualified Data.Map.Strict as Map
+import Data.Map as Map
+
+from_just (Just a) = a
 
 {-
  - Insere um elemento na estrutura. Caso o elemento ja existe, sua quantidade na estrutura sera incrementada.
  -}
-insert elem bag = undefined
+insert_bag elem bag 
+  | amount == Nothing = insert elem 1 bag
+  | otherwise = insert elem ((from_just amount) + 1) bag
+  where
+    amount = Map.lookup elem bag
 
 {-
 - Remove um elemento da estrutura, levando em consideracao a manipulacao de sua quantidade na estrutura. 
 - Caso a quantidade atinja 0 (ou menos), o elemento deve realmente ser removido da estrutura
 -}
-remove elem bag = undefined
+--remove elem bag = undefined
 
 {-
  - Busca um elemento na estrutura retornando sua quantidade. Caso o elemento nao exista, retorna 0 como a quantidade.
 -}
-search elem bag = undefined
+--search elem bag = undefined
 
 {-
  - Faz a uniao deste Bag com otherBag. A uniao consiste em ter os elementos dos dois Bags com suas maiores quantidades.
  - Por exemplo, A = {(a,1),(c,3)}, B = {(b,2),(c,1)}. A.union(B) deixa A = {(a,1),(c,3),(b,2)}
 -}
-union bag1 bag2 = undefined
+--union bag1 bag2 = undefined
 
 {-
  - Faz a intersecao deste Bag com otherBag. A intersecao consiste em ter os elementos que estao em ambos os bags com suas 
  - menores quantidades. Por exemplo, Seja A = {(a,3),(b,1)} e B = {(a,1)}. Assim, A.intersection(B) deixa A = {(a,1)}
  - Caso senhum elemento de A esteja contido em B ent�o a intersecao deixa A vazio.
 -}
-intersection bag1 bag2 = undefined
+--intersection bag1 bag2 = undefined
 
 {-
  - Faz a diferenca deste Bag com otherBag. A diferenca A \ B entre bags eh definida como segue:
@@ -48,20 +54,20 @@ intersection bag1 bag2 = undefined
      Caso essa quantidade seja negativa o elemento deve serremovido do Bag. 
      Por exemplo, seja A = {(a,3),(b,1)} e B = {(b,2),(a,1)}. Assim, A.minus(B) deixa A = {(a,2)}.
 -}
-minus bag1 bag2 = undefined
+--minus bag1 bag2 = undefined
 
 {-
  - Testa se este Bag esta incluso em otherBag. Para todo elemento deste bag, sua quantidade
  - deve ser menor or igual a sua quantidade em otherBag.
 -}
-inclusion bag1 bag2 = undefined
+--inclusion bag1 bag2 = undefined
 
 {-
  - Realiza a soma deste Bag com otherBag. A soma de dois bags contem os elementos dos dois bags com suas quantidades somadas. 
 -}
-sum bag1 bag2 = undefined
+--sum bag1 bag2 = undefined
 
 {-
  - Retorna a quantidade total de elementos no Bag
 -}
-size bag = undefined
+--size bag = undefined
